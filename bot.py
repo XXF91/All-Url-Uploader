ca73f33a5cbf460fa8db4eb607fe9da9a2befe8c
XXF91 <mrsjoaker@gmail.com>
2025-02-03 12:17:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,28 +5,6 @@ from pyrogram import Client, idle, __version__
 
 from config import Config
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-    os.makedirs(Config.DOWNLOAD_LOCATION)
-
-if not Config.BOT_TOKEN:
-    logger.error("Please set BOT_TOKEN in config.py or as env var")
-    quit(1)
-
-if not Config.API_ID:
-    logger.error("Please set API_ID in config.py or as env var")
-    quit(1)
-
-if not Config.API_HASH:
-    logger.error("Please set API_HASH in config.py or as env var")
-    quit(1)
-
 
 bot = Client(
     "All-Url-Uploader",
